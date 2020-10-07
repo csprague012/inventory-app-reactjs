b01201e0073829b36a2ae674ee28bfbc2d19653a
Chris Sprague <chris@desktopshipper.com>
2020-10-07 16:51:54 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello Idiot
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+  componentDidMount(){
+    let headers = new Headers();
+    headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+    headers.append('Access-Control-Allow-Credentials', 'true');
+    headers.append('GET', 'POST', 'OPTIONS');
+    fetch('https://inventoryappdemo.herokuapp.com/Controllers/HomeController/Init')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        });
+      });
+
+  }
+
+  render(){
+    var {isLoaded, items} = this.state;
+    if(!isLoaded){
+      return <div>Loading...</div>;
+    }
+    else{    
+      return (
+        <div className="App">
+          Hello Idiot
+        </div>
+      );
+    }
+  }
+
 }
 
 export default App;
