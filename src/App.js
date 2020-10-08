d14743a2aa909c73670042f0c99fa03656f996ec
Chris Sprague <chris@desktopshipper.com>
2020-10-07 22:22:54 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import './App.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faPencilAlt } from '@fortawesome/free-solid-svg-icons'
 class App extends Component{
   constructor(props){
     super(props);
@@ -11,13 +11,13 @@ class App extends Component{
   }
   componentDidMount(){
     let headers = new Headers();
-    headers.append('Access-Control-Allow-Origin', '*');
+    headers.append('Access-Control-Allow-Origin', '');
     fetch('https://inventoryappdemo.herokuapp.com/Controllers/HomeController/Init')
       .then(res => res.json())
       .then(json => {
         this.setState({
           isLoaded: true,
-          items: json,
+          items: json.items,
         });
       });
 
@@ -30,8 +30,27 @@ class App extends Component{
     }
     else{    
       return (
-        <div className="App">
-          Hello Idiot
+        <div className="container">
+          <table className="table table-striped" style={{tableLayout: "fixed"}}>
+            <thead>
+            <tr>
+              <th width="15%"><button className="btn btn-sm btn-success"><FontAwesomeIcon icon={faPlus}/></button></th>
+              <th wdith="10%">Id</th>
+              <th width="55%">Name</th>
+              <th width="25%">Cost</th>
+            </tr>
+            </thead>
+            <tbody>
+            {items.map(item => (
+              <tr key={item.id}>
+                <td><button className="btn btn-sm btn-info"><FontAwesomeIcon icon={faPencilAlt}/></button></td>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.cost}</td>
+              </tr>
+            ))}
+            </tbody>
+          </table>
         </div>
       );
     }
